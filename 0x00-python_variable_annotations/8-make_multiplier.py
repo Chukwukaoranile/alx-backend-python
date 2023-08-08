d2e542a13e287f0188da1f9ce02fbc5c3fcb73e1
chukwukaoranile <prosperbinary@gmail.com>
2023-08-08 15:44:47 +0100

    fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Write a type-annotated function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier.
-"""
+""" A type-annotated function make_multiplier that takes a float multiplier """
 
 from typing import Callable
 
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Annotate the below function’s parameters and return values with the appropriate types"""
+""" Annotate the below function’s parameters and return values """
 
 from typing import List, Tuple, Sequence, Iterable
 
